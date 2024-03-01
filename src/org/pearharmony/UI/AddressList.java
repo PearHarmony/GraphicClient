19b5b2c128c6864bdc2f33b35a729e12a71f7e3e
DWRudi <136797425+DWRudi@users.noreply.github.com>
2024-03-01 09:37:56 +0100

    update addlist

--- src/org/pearharmony/UI/AddressList.java
+++ src/org/pearharmony/UI/AddressList.java
@@ -3,6 +3,7 @@ package org.pearharmony.UI;
 import javax.swing.*;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -14,6 +15,10 @@ public class AddressList extends JPanel implements ActionListener{
     private Map<JButton, String> addressList = new HashMap<>();
     JPanel AddrSelList = new JPanel();
 
+    JTextField newAddrName = new JTextField(8);
+    JTextField newAddrIP = new JTextField(8);
+    JButton addNewIndex = new JButton("Add");
+
     public AddressList(Messager messager) {
         this.messager = messager;
         setBounds(0, 0, 300, 700);
@@ -22,18 +27,35 @@ public class AddressList extends JPanel implements ActionListener{
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        JButton name = new JButton("Addresses");
+        
+        
 
         AddrSelList.setLayout(new BoxLayout(AddrSelList, BoxLayout.Y_AXIS));
 
         AddAddress("localhost", "localhost");
 
+        JTextField name = new JTextField("Addresses");
+        name.setMaximumSize(new Dimension(400, 20));
+
         JScrollPane list = new JScrollPane(AddrSelList);
         list.setWheelScrollingEnabled(true);
         list.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
+        JPanel addPanel = new JPanel();
+        addPanel.setMaximumSize(new Dimension(400, 20));
+
+        newAddrName.setToolTipText("Name");
+        newAddrIP.setToolTipText("IP");
+
+        addNewIndex.addActionListener(this);
+
+        addPanel.add(newAddrName);
+        addPanel.add(newAddrIP);
+        addPanel.add(addNewIndex);
+
         add(name);
         add(list);
+        add(addPanel);
     }
 
     public void UpdateAddressList(){
@@ -41,6 +63,7 @@ public class AddressList extends JPanel implements ActionListener{
         for (JButton button : addressList.keySet()) {
             AddrSelList.add(button);
         }
+        revalidate();
     }
 
     public void AddAddress(String name, String address){
@@ -53,6 +76,18 @@ public class AddressList extends JPanel implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        messager.SetAddres(addressList.get(e.getSource()));
+        if(addressList.containsKey(e.getSource())){
+            messager.SetAddres(addressList.get(e.getSource()));
+        }
+        else if(e.getSource() == addNewIndex)
+        {
+            if(newAddrName.getText().length() > 0 && newAddrIP.getText().length() > 0)
+            {
+                System.out.println("asd");
+                AddAddress(newAddrName.getText(), newAddrIP.getText());
+                newAddrName.setText("");
+                newAddrIP.setText("");
+            }
+        }
     }
 }
